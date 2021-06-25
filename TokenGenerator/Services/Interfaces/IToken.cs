d2089009ff9ef8d92797511484f83f1df360081c
Jon Kjetil Øye <acn-joye@digdir.no>
2021-06-25 14:32:39 +0200

    Added support for Supplier claim in EC user token In order to test delegated access for scopes in the Altinn REST API when we open for use of EC user tokens for "enterprise authenticated" operations.

--- TokenGenerator/Services/Interfaces/IToken.cs
+++ TokenGenerator/Services/Interfaces/IToken.cs
@@ -5,7 +5,7 @@ namespace TokenGenerator.Services
     public interface IToken
     {
         Task<string> GetEnterpriseToken(string env, string[] scopes, string org, string orgNo, string supplierOrgNo, uint ttl);
-        Task<string> GetEnterpriseUserToken(string env, string[] scopes, string org, string orgNo, uint partyId, uint userId, string userName, uint ttl);
+        Task<string> GetEnterpriseUserToken(string env, string[] scopes, string org, string orgNo, string supplierOrgNo, uint partyId, uint userId, string userName, uint ttl);
         Task<string> GetPersonalToken(string env, string[] scopes, uint userId, uint partyId, string pid, string authLvl, string consumerOrgNo, string userName, string client_amr, uint ttl);
         string Dump(string token);
         bool IsValidAuthLvl(string authlvl);
