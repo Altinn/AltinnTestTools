071f4a1a0d368e10214c5c2e590bfd723288a8e9
Bjørn Dybvik Langfors <bdl@digdir.no>
2025-02-06 21:54:57 +0100

    Handle path weirdness

--- TokenGenerator/Services/RandomIdentifier.cs
+++ TokenGenerator/Services/RandomIdentifier.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 using TokenGenerator.Services.Interfaces;
 
 namespace TokenGenerator.Services;
 
 public class RandomIdentifier : IRandomIdentifier
 {
+    private readonly ILogger<RandomIdentifier> logger;
     private string[] randomPersonalIdentifiers;
     private string[] randomEnterpriseIdentifiers;
     
-    public RandomIdentifier()
+    public RandomIdentifier(ILogger<RandomIdentifier> logger)
     {
+        this.logger = logger;
         LoadData();
     }
     
@@ -39,7 +43,28 @@ public class RandomIdentifier : IRandomIdentifier
     
     private void LoadData()
     {
-        randomPersonalIdentifiers = System.IO.File.ReadAllLines("Data/endusers.txt");
-        randomEnterpriseIdentifiers = System.IO.File.ReadAllLines("Data/enterprises.txt");
+        string basePath = Environment.GetEnvironmentVariable("DEPLOYMENT_TARGET") ?? "";
+        string endusersPath = Path.Combine(basePath, "Data", "endusers.txt");
+        string enterprisesPath = Path.Combine(basePath, "Data", "enterprises.txt");
+
+        if (File.Exists(endusersPath))
+        {
+            randomPersonalIdentifiers = File.ReadAllLines(endusersPath);
+        }
+        else
+        {
+            logger.LogWarning($"Could not find file with random personal identifiers at path: {endusersPath}");
+            randomPersonalIdentifiers = Array.Empty<string>();
+        }
+        
+        if (File.Exists(enterprisesPath))
+        {
+            randomEnterpriseIdentifiers = File.ReadAllLines(enterprisesPath);
+        }
+        else
+        {
+            logger.LogWarning($"Could not find file with random enterprise identifiers at path: {enterprisesPath}");
+            randomEnterpriseIdentifiers = Array.Empty<string>();
+        }
     }
 }
