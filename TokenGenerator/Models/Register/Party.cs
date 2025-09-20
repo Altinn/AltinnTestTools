96e63048473aa7e652109f98f28f628464560574
Jon Kjetil Øye <acn-joye@ai-dev.no>
2025-09-20 18:48:05 +0200

    feat: add support for lookup of internal ids for personal tokens - added RegisterService and models for Pid lookup - added new url param: getEnvIds, for requesting pid lookup - added needed config for APIM subscription keys

---
+++ TokenGenerator/Models/Register/Party.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Altinn.Register.Models;
+
+/// <summary>
+/// Represents a party in Altinn Register.
+/// </summary>
+public class Party
+{
+    /// <summary>
+    /// Gets the type of the party.
+    /// </summary>
+    [JsonPropertyName("partyType")]
+    public string Type { get; }
+
+    /// <summary>
+    /// Gets the type of the party.
+    /// </summary>
+    [JsonPropertyName("personIdentifier")]
+    public string Pid { get; }
+
+    /// <summary>
+    /// Gets the UUID of the party.
+    /// </summary>
+    [JsonPropertyName("partyUuid")]
+    public Guid Uuid { get; init; }
+
+    /// <summary>
+    /// Gets the version ID of the party.
+    /// </summary>
+    [JsonPropertyName("versionId")]
+    public ulong VersionId { get; init; }
+
+    /// <summary>
+    /// Gets the canonical URN of the party.
+    /// </summary>
+    [JsonPropertyName("urn")]
+    public string Urn { get; init; }
+
+    /// <summary>
+    /// Gets the ID of the party.
+    /// </summary>
+    [JsonPropertyName("partyId")]
+    public uint PartyId { get; init; }
+
+    /// <summary>
+    /// Gets the user ID of the party.
+    /// </summary>
+    [JsonPropertyName("userId")]
+    public uint UserId { get; init; }
+
+    /// <summary>
+    /// Gets the display-name of the party.
+    /// </summary>
+    [JsonPropertyName("displayName")]
+    public string DisplayName { get; init; }
+}
---
+++ TokenGenerator/Services/Interfaces/IRegisterService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TokenGenerator.Services.Interfaces
+{
+    public interface IRegisterService
+    {
+        Task<(bool Success, uint UserId, uint PartyId, Guid PartyUuid)> GetEnvironmentIdentifiers(string env, string pid, string platformAccessToken, string subscriptionKey, CancellationToken cancellationToken);
+    }
+}
